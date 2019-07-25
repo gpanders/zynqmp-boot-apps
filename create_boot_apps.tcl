e1555e0ab939c1dc2f33f09838ffe93840dba96d
Greg Anders <greg@gpanders.com>
2019-07-25 09:22:08 -0700

    Delete build/device_tree directory before copying

--- create_boot_apps.tcl
+++ create_boot_apps.tcl
@@ -9,13 +9,13 @@ if { $::argc > 0 } {
     set xilinx_version "xilinx-v[lindex $::argv 0]"
 }
 
+file delete -force build/workspace
+setws build/workspace
+
 file delete -force build/device-tree-xlnx
 exec -ignorestderr git clone -q --branch $xilinx_version https://github.com/Xilinx/device-tree-xlnx build/device-tree-xlnx
-
-file delete -force build/workspace
-
-setws build/workspace
 repo -set build/device-tree-xlnx
+
 createhw -name $hwproject -hwspec build/$hwspec
 createapp -name fsbl -hwproject $hwproject -proc psu_cortexa53_0 -app {Zynq MP FSBL} -os standalone
 configapp -app fsbl define-compiler-symbols FSBL_DEBUG_DETAILED
@@ -25,10 +25,11 @@ configbsp -bsp device_tree periph_type_overrides "{BOARD zcu102-rev1.0}"
 regenbsp -bsp device_tree
 projects -build
 
-# Copy all outputu products to top level build directory
-file copy -force build/workspace/device_tree build/device_tree
-file copy -force build/workspace/fsbl/Debug/fsbl.elf build/fsbl.elf
-file copy -force build/workspace/pmufw/Debug/pmufw.elf build/pmufw.elf
-file copy -force [glob build/workspace/$hwproject/*.bit] build/bitstream.bit
+# Copy all output products to top level build directory
+file delete -force build/device_tree
+file copy -force build/workspace/device_tree build/
+file copy -force build/workspace/fsbl/Debug/fsbl.elf build/
+file copy -force build/workspace/pmufw/Debug/pmufw.elf build/
+file copy -force [glob build/workspace/$hwproject/*.bit] build/
 
 exit
